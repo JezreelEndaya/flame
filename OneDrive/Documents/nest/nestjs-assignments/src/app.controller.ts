826a8e969c5aea73396700a5041308cda36b1006
JezreelEndaya <jezreelhazel.endaya@cvsu.edu.ph>
2024-11-09 17:56:41 +0800

    Completed Assignment 2: Prime Number

--- OneDrive/Documents/nest/nestjs-assignments/src/app.controller.ts
+++ OneDrive/Documents/nest/nestjs-assignments/src/app.controller.ts
@@ -12,17 +12,38 @@ export class AppController {
 
   @Get("assignment-1-fibonacci-sequence/:terms")
   getFibonacciSequence(@Param("terms") terms: string): { sequence: number[] } {
-  const numberOfTerms = parseInt(terms, 10); 
-  const sequence = [0, 1];
+    const numberOfTerms = parseInt(terms, 10); 
+    const sequence = [0, 1];
 
-  for (let i = 2; i < numberOfTerms; i++) {
-    sequence.push(sequence[i - 1] + sequence[i - 2]);
+    for (let i = 2; i < numberOfTerms; i++) {
+      sequence.push(sequence[i - 1] + sequence[i - 2]);
+    }
+
+    return { 
+      "sequence" : sequence 
+    };
   }
 
-  return { 
-    "sequence" : sequence 
-  };
+  @Get("assignment-2-prime-number/:number")
+  getPrimeNumber(@Param('number') number: string): { isPrime: boolean } {
+    const num = parseInt(number);
 
+    if (isNaN(num)) {
+      return { isPrime: false };
+    }
+
+    if (num <= 1) {
+      return { isPrime: false };
+    }
+
+    for (let i = 2; i < Math.floor(num); i++) {
+      if (num % i === 0) {
+        return { isPrime: false };
+      }
+    }
+
+    return { 
+      "isPrime" : true 
+    };
   }
-
 }
